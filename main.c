383340143b3f03fb73ab8e2321acd2e4e14d2e43
KarolWes <karol.wesolowski01@gmail.com>
2023-05-20 14:21:31 +0200

    parser for tree part 5 -> printout part 8 -> accepting global setting from console

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
+#include <stdlib.h>
 #include "printout.h"
 
-int main() {
-    run(1);
+int main(int argc, char *argv[]) {
+    if(argc != 2)
+    {
+        run(0);
+    }
+    else{
+        run(atoi(argv[1]));
+    }
     return 0;
 }
